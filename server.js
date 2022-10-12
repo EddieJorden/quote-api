fec469fb882b0f982af41f6ca5171c21a869c481
EddieJorden <eddie@eddiejorden.tech>
2022-10-12 08:48:42 -0700

    get random quote route working

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 const { quotes } = require('./data');
 const { getRandomElement } = require('./utils');
 
+app.get(`/api/quotes/random`, (req, res) => {
+	const randomQuote = getRandomElement(quotes);
+	const quote = {
+		quote: randomQuote,
+	};
+	console.log(`sending`, quote);
+	res.send(quote);
+});
+
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
